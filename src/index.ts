4309092488580939eda58da778f5ebe52873fafc
Kimmo Lepola <kimmo.lepola@gmail.com>
2020-10-25 17:54:43 +0200

    Heroku

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import patientRouter from './routes/patients';
 import diagnoseRouter from './routes/diagnoses';
 import express from 'express';
+require('dotenv').config();
+const { PORT } = process.env;
+
 const cors = require('cors'); // eslint-disable-line
 const app = express();
 app.use(express.json());
@@ -8,7 +11,7 @@ app.use(cors()); // eslint-disable-line
 app.use('/api/diagnosis', diagnoseRouter);
 app.use('/api/patients', patientRouter);
 
-const PORT = 3001;
+//const PORT = 3001;
 
 app.get('/api/ping/', (_req, res) => {
     console.log('pinged');
